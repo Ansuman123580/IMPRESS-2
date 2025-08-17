021fcf9720c4a97b52619e62fef49b7a2a5e0862
Ansuman Maharana <144708198+Ansuman123580@users.noreply.github.com>
2025-08-17 12:44:41 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -45,7 +45,7 @@ const PlaceOrder = () => {
                             <b>₹{getTotalCartAmount() + 2}</b>
                         </div>
                     </div>
-                    <button >PROCEED TO PAYMENT</button>
+                    <button class="pay-btn">PROCEED TO PAYMENT</button>
                 </div>
             </div>
         </form>
