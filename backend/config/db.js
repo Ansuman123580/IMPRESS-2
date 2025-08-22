e94e3e0de2384b28208dfae3caa08810c5c3aafd
Ansuman Maharana <144708198+Ansuman123580@users.noreply.github.com>
2025-08-22 19:09:41 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://storeimpress:Impress12313@cluster0.wz4q1dl.mongodb.net/impresss').then(()=>console.log("DB Connected"));
-}
